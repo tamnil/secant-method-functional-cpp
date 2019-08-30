11edbb3c9aa073434e338111a08a63e02381b2b6
Tamnil Saito Junior <tamnil@gmail.com>
2019-08-30 00:30:17 -0300

    templates correction

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+g++ ./src/secante.c -o ./bin/secante.exe
+./bin/secante.exe
--- src/secante.c
+++ src/secante.c
@@ -1,45 +1,51 @@
 /*
-* Tamnil Saito Junior 2019
-*
-* please use c++ >= v11.x
-* tested with g++ - linux-ubuntu
-*/
+ * Tamnil Saito Junior 2019
+ *
+ * please use c++ >= v11.x
+ * tested with g++ - linux-ubuntu
+ */
 
 #include <functional>
 #include <math.h>
 #include <iostream>
 
-//basic equations:
+/* basic equations: */
 
 
 auto fnSqureRootOfTwo = [](double x)->double{
-    return exp2f(3) - 2;
+    return pow(x,2) - 2;
 };
 auto calculatedError = [](double x1, double x2)->double{
     return  abs( (x2 - x1)/x2 );
 };
 auto delta = [](double x0,double x1 ) -> double {
-return x1 - x0;
+    return x1 - x0;
 };
 auto nextStepSecant = [](auto fn, double x0, double x1 )->double{
     return   x1 - ( fn(x1) * delta(x0,x1) / delta(fn(x0), fn(x1) ) );
 };
 
-
-
-// templating:
-auto printToOutput = [] (double x,  char* prefix = "") -> bool{ printf("Result: %.52f \n",x); };
-
-//main equation:
+/* main equation: */
 auto secantMethod = [](auto fn, double x0,double x1,double errorMax) -> double {
     double x2  = nextStepSecant(fn,x0,x1);
     return  (calculatedError(x1,x2) > errorMax) ? secantMethod(fn, x1, x2, errorMax) : x2;
 };
 
-// not necessary fot root two:
+
+
+
+
+/*  not necessary fot root two: */
+
+/*  templating: */
+auto printToOutput = [] (double x,  const char* prefix = "") -> bool{
+    printf("Result - %s: %.52f \n", prefix ,x);
+};
+
+
 
 auto fnCubicRoot = [](double x) -> double{
-    return (x * x * x) -2;
+    return (x * x * x) -3;
 };
 
 
@@ -51,14 +57,10 @@ int main(){
            errorMax = 0.1E-16;
     /* end of initial setup */
 
-    /* get teh root of two: */
+    /* get the root of two: */
     double rootOfTwo = secantMethod( fnSqureRootOfTwo, x0, x1, errorMax);
     /* print it */
-    printToOutput(rootOfTwo);
-
-    /* std::cout << exp2(2); */
-      std::cout << "main function\n";
-
+    printToOutput(rootOfTwo,"root of two");
 
     /* lets test with other funtions */
 
